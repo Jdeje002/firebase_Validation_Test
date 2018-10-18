8881126ca9b9a1c31c39c964815b3f06000155bf
John De Jesus <JDeJesus1@lahd.ci.la.ca.us>
2018-10-18 08:26:46 -0700

    initalizing firebase

---
+++ index.js
@@ -0,0 +1,12 @@
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyCj6QM7xcyqAP4utAxp7-q7cZiwzQyVdLU",
+    authDomain: "fir-validation-test.firebaseapp.com",
+    databaseURL: "https://fir-validation-test.firebaseio.com",
+    projectId: "fir-validation-test",
+    storageBucket: "fir-validation-test.appspot.com",
+    messagingSenderId: "1096452691234"
+  };
+  firebase.initializeApp(config);
+
+  
